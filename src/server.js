0395227171fc714e9685de15f4de44a591044e30
Nicolas Santana <nicolassantanadev@gmail.com>
2025-02-12 21:41:19 -0300

    adding and displaying users

--- src/server.js
+++ src/server.js
@@ -8,8 +8,19 @@ const io = socketIo(server);
 
 const port = process.env.PORT || 4000;
 
+const users = [] ;
+
 io.on('connection', (socket) =>{
-  console.log(socket.id)
+  socket.on("disconnect", () => {
+  
+  })
+
+ socket.on("join", (name) => {
+  const user = {id: socket.id, name};
+  users.push(user);
+  io.emit("message", {name: null, message: `${name} entrou no chat`})
+  io.emit("users", users)
+ })
 })
 
 server.listen(port, () => console.log(`servidor rodando na porta ${port}`))
